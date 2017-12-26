d46e5e360c4b423bb43fc723afe0af07645b24f3
pd <pd@pds-MacBook-Pro.local>
2017-12-26 14:46:01 +0800

    2017-12-26:Initial

---
+++ server.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if (error) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  next();
+});
+
+//app.use((req, res, next) => {
+//  res.render('maintainance.hbs');
+//});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    authorName: 'Pd',
+    address: 'Shenzhen, China',
+    pageTitle: 'Homepage',
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorCode: 404,
+    errorMsg: 'Cannot find the page',
+    suggestion: 'choose another page to visit'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is up on 3000');
+});
